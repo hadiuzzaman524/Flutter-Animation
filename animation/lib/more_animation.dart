29a9109387dfc82df015a1d36df1e3a965e2f4e3
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2021-01-29 16:49:39 +0600

    learn about AnimatedContainer

--- animation/lib/more_animation.dart
+++ animation/lib/more_animation.dart
@@ -1,18 +1,74 @@
 import 'package:flutter/material.dart';
 
 class MoreAnimation extends StatefulWidget {
-  static const routeName='/MoreAnimation';
+  static const routeName = '/MoreAnimation';
 
   @override
   _MoreAnimationState createState() => _MoreAnimationState();
 }
 
 class _MoreAnimationState extends State<MoreAnimation> {
+  double value = 0;
+  double _opacity = 1;
+  Color color=Colors.red;
+  
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: AnimatedContainer(
+          margin: EdgeInsets.all(value),
+          height: 500,
+          width: 300,
+          color: color,
+          duration: Duration(seconds: 1),
+          child: Column(
+            children: [
+              RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    value = 50;
+                  });
+                },
+                child: Text('More Margin'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    value = 0;
+                  });
+                },
+                child: Text('Less Margin'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  setState(() {
+                   color=Colors.green;
+                  });
+                },
+                child: Text('Change Color '),
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    _opacity = 0;
+                  });
+                },
+                child: Opacity(
+                  opacity: _opacity,
+                  child: Text(
+                    'Hide Me',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
